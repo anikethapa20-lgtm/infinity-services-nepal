aabcef8781904ecd4c9464b8c5090f91171194fa
anikethapa20-lgtm <anikethapa20@gmail.com>
2026-06-20 00:39:19 -0500

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -8,19 +8,25 @@ import ServiceDetail from './pages/ServiceDetail'
 import OurWork from './pages/OurWork'
 import Award from './pages/Award'
 import Contact from './pages/Contact'
+import AdminMedia from './pages/AdminMedia'
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/:slug" element={<ServiceDetail />} />
-        <Route path="/our-work" element={<OurWork />} />
-        <Route path="/global-authors-award" element={<Award />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/:slug" element={<ServiceDetail />} />
+          <Route path="/our-work" element={<OurWork />} />
+          <Route path="/global-authors-award" element={<Award />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<AdminMedia />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
